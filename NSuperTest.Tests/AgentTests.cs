6d096e3a3aee5533684ba80f362862cf78e7ee91
Peter Short <peter.short@gmail.com>
2016-04-27 17:34:34 +0000

    Changed the test project to use nunit
    This is so travis CI can run the build and possibly tests for me

---
+++ NSuperTest.Tests/AgentTests.cs
@@ -0,0 +1,167 @@
+﻿using Moq;
+using NSuperTest;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NSuperTestTests
+{
+    [TestFixture]
+    public class AgentTests
+    {
+        Mock<IHttpRequestClient> client;
+        ITestBuilder builder;
+        HttpRequestMessage message;
+
+        [SetUp]
+        public void Init()
+        {
+            client = new Mock<IHttpRequestClient>();
+            client.Setup(c => c.MakeRequest(It.IsAny<HttpRequestMessage>()))
+                .Callback<HttpRequestMessage>(r => message = r);
+
+            builder = new TestBuilder("/test", client.Object);
+        }
+
+        [Test]
+        public void ShouldPerformGet()
+        {
+            builder.SetMethod(HttpMethod.Get);
+
+            builder
+                .End();
+
+            client.Verify(
+                c => c.MakeRequest(It.Is<HttpRequestMessage>(m => 
+                    m.Method == HttpMethod.Get && m.RequestUri.OriginalString == "/test")), 
+                Times.Once());
+        }
+
+        [Test]
+        public void ShouldPerformPost()
+        {
+            builder.SetMethod(HttpMethod.Post);
+
+            builder
+                .End();
+
+            client.Verify(
+                c => c.MakeRequest(It.Is<HttpRequestMessage>(m =>
+                    m.Method == HttpMethod.Post && m.RequestUri.OriginalString == "/test")),
+                Times.Once());
+        }
+
+        [Test]
+        public void ShouldPerformPut()
+        {
+            builder.SetMethod(HttpMethod.Put);
+
+            builder
+                .End();
+
+            client.Verify(
+                c => c.MakeRequest(It.Is<HttpRequestMessage>(m =>
+                    m.Method == HttpMethod.Put && m.RequestUri.OriginalString == "/test")),
+                Times.Once());
+        }
+
+        [Test]
+        public void ShouldPerformDelete()
+        {
+            builder.SetMethod(HttpMethod.Delete);
+
+            builder
+                .End();
+
+            client.Verify(
+                c => c.MakeRequest(It.Is<HttpRequestMessage>(m =>
+                    m.Method == HttpMethod.Delete && m.RequestUri.OriginalString == "/test")),
+                Times.Once());
+        }
+
+        [Test]
+        public void ShouldSetHeader()
+        {
+            builder.SetMethod(HttpMethod.Post);
+
+            builder
+                .Set("TestHeader", "TestValue")
+                .End();
+
+            client.Verify(
+                c => c.MakeRequest(It.Is<HttpRequestMessage>(m =>
+                    m.Headers.GetValues("TestHeader").Single() == "TestValue")),
+                Times.Once());
+        }
+
+        [Test]
+        public void ShouldSetBearerToken()
+        {
+            builder.SetMethod(HttpMethod.Get);
+
+            builder
+                .SetBearerToken("test")
+                .End();
+
+            client.Verify(
+                c => c.MakeRequest(It.Is<HttpRequestMessage>(m =>
+                    m.Headers.GetValues("Authorization").Single() == "Bearer test")),
+                Times.Once());
+        }
+
+        [Test]
+        public void ShouldSetBearerByFuncWithString()
+        {
+            builder.SetMethod(HttpMethod.Get);
+
+            builder
+                .SetBearerToken(() => "hello")
+                .End();
+
+            client.Verify(
+                c => c.MakeRequest(It.Is<HttpRequestMessage>(m =>
+                m.Headers.GetValues("Authorization").Single() == "Bearer hello")),
+                Times.Once());
+        }
+
+        [Test]
+        public void ShouldSetBearerByFuncWithTaskOfString()
+        {
+            builder.SetMethod(HttpMethod.Get);
+
+            builder
+                .SetBearerToken(async () =>
+                {
+                    return await Task.FromResult("hello");
+                })
+                .End();
+
+
+            client.Verify(
+                c => c.MakeRequest(It.Is<HttpRequestMessage>(m =>
+                m.Headers.GetValues("Authorization").Single() == "Bearer hello")),
+                Times.Once());
+        }
+
+        [Test]
+        public void ShouldSetPostBody()
+        {
+            builder.SetMethod(HttpMethod.Post);
+
+            builder
+                .Send(new { Name = "Peter", Age = 32 })
+                .End();
+
+            client.Verify(
+                c => c.MakeRequest(It.Is<HttpRequestMessage>(m =>
+                        m.Content.ReadAsStringAsync().Result == "{\"Name\":\"Peter\",\"Age\":32}"
+                        && m.Content.Headers.ContentType.MediaType == "application/json"
+                    )), Times.Once());
+        }
+    }
+}
---
+++ NSuperTest.Tests/AssertionTests.cs
@@ -0,0 +1,178 @@
+﻿using NSuperTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace NSuperTestTests
+{
+    [TestFixture]
+    public class AssertionTests
+    {
+        HttpResponseMessage message;
+        TestBuilder builder;
+        Mock<IHttpRequestClient> clientMock;
+
+        User user;
+
+        [SetUp]
+        public void Init()
+        {
+            message = new HttpResponseMessage();
+            message.StatusCode = HttpStatusCode.OK;
+            message.Content = new StringContent("Hello World");
+            message.Headers.Add("TestHeader", "Test");
+
+            user = new User { Name = "Peter", Age = 32, Id = 1 };
+
+            clientMock = new Mock<IHttpRequestClient>();
+            clientMock.Setup(c => c.MakeRequest(It.IsAny<HttpRequestMessage>())).Returns(message);
+
+            builder = new TestBuilder("/test", clientMock.Object);
+            builder.SetMethod(HttpMethod.Get);
+        }
+
+        [Test]
+        public void ShouldAssertStatusCodes()
+        {
+            builder
+                .Expect(200)
+                .End();
+        }
+
+        [Test]
+        public void ShouldThrowAssertStatusCodes()
+        {
+            Action action = () => builder.Expect(401).End();
+            action.ShouldThrow<Exception>()
+                .WithMessage("Expected status code 401 but got 200");
+        }
+
+        [Test]
+        public void ShouldAssertStatusAndCallback()
+        {
+            builder
+                .Expect(200, m =>
+                {
+                    m.StatusCode.ShouldBeEquivalentTo<HttpStatusCode>(HttpStatusCode.OK);
+                });
+        }
+
+        [Test]
+        public void ShouldAssertBody()
+        {
+            builder
+                .Expect("Hello World")
+                .End();
+        }
+
+        [Test]
+        public void ShouldThrowBadBody()
+        {
+            Action a = () => builder.Expect("Goodbye World").End();
+
+            a.ShouldThrow<Exception>()
+                .WithMessage("Expected body 'Goodbye World' but got 'Hello World'");
+        }
+
+        [Test]
+        public void ShouldAssertBodyAndCallback()
+        {
+            builder
+                .Expect("Hello World", m =>
+                {
+                    m.Content.ReadAsStringAsync().Result.Should().StartWith("H");
+                });
+        }
+
+        [Test]
+        public void ShouldAssertHeaders()
+        {
+            builder
+                .Expect("TestHeader", "Test")
+                .End();
+        }
+
+        [Test]
+        public void ShouldThrowBadHeaderName()
+        {
+            Action a = () => builder.Expect("Content", "100").End();
+            a.ShouldThrow<Exception>().WithMessage("Header 'Content' not found on response message");
+        }
+
+        [Test]
+        public void ShouldThrowBadHeaderValue()
+        {
+            Action a = () => builder.Expect("TestHeader", "100").End();
+            a.ShouldThrow<Exception>().WithMessage("Header 'TestHeader' not found with value '100' on response message");
+        }
+
+        [Test]
+        public void ShouldAssertHeaderWithCallback()
+        {
+            builder
+                .Expect("TestHeader", "Test", m =>
+                {
+                    m.Headers.GetValues("TestHeader").First().Should().BeSameAs("Test");
+                    m.Headers.GetValues("TestHeader").Should().HaveCount(1);
+                });
+        }
+
+        [Test]
+        public void ShouldAssertCallback()
+        {
+            builder
+                .Expect(m =>
+                {
+                    m.Content.ReadAsStringAsync().Result.Should().Contain("Hello World");
+                    m.StatusCode.ShouldBeEquivalentTo<HttpStatusCode>(HttpStatusCode.OK);
+                })
+                .End();
+        }
+
+        [Test]
+        public void ShouldAssertAnObjectBody()
+        {
+            message.Content = new StringContent(JsonConvert.SerializeObject(user));
+            clientMock.Setup(c => c.MakeRequest(It.IsAny<HttpRequestMessage>())).Returns(message);
+
+            builder
+                .Expect(user)
+                .End();
+        }
+
+        [Test]
+        public void ShouldThrowBadObjectBody()
+        {
+            message.Content = new StringContent(JsonConvert.SerializeObject(user));
+            clientMock.Setup(c => c.MakeRequest(It.IsAny<HttpRequestMessage>())).Returns(message);
+
+            Action a = () => builder
+                                .Expect(new User { Name = "Tom", Age = 11, Id = 1 })
+                                .End();
+
+            a.ShouldThrow<Exception>();
+        }
+
+        [Test]
+        public void ShouldAssertAnObjectBodyAndCallback()
+        {
+            message.Content = new StringContent(JsonConvert.SerializeObject(user));
+            clientMock.Setup(c => c.MakeRequest(It.IsAny<HttpRequestMessage>())).Returns(message);
+
+            builder
+                .Expect(user, m =>
+                {
+                    m.StatusCode.ShouldBeEquivalentTo<HttpStatusCode>(HttpStatusCode.OK);
+                });
+        }
+
+    }
+}
