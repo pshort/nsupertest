1eb92bd19f622ed7ce8bdc09b6b2ed3a3dcd116d
Gísli Konráð Björnsson <gisli.konrad@gmail.com>
2016-10-25 17:13:37 +0000

    removes unneccesary usings in Server

--- NSuperTest/Server.cs
+++ NSuperTest/Server.cs
@@ -1,14 +1,9 @@
 ﻿using Microsoft.Owin.Host.HttpListener;
 using Microsoft.Owin.Hosting;
 using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NSuperTest
 {
