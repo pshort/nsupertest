a1c8327058379e5376c217dbfbe457c265830816
Peter Short <peter.short@gmail.com>
2017-05-28 14:59:26 +0000

    working on scripts

--- build.ps1
+++ build.ps1
@@ -1,2 +1,4 @@
 dotnet build ./NSuperTest/NSuperTest.csproj
 dotnet build ./NSuperTest.Tests/NSuperTest.Tests.csproj
+dotnet build ./TestWebApi/TestApi/TestApi.csproj
+dotnet build ./TestWebApi/TestApi.Tests/TestApi.Tests.csproj
---
+++ restore.ps1
@@ -0,0 +1,4 @@
+dotnet restore NSuperTest/NSuperTest.csproj
+dotnet restore NSuperTest.Tests/NSuperTest.Tests.csproj
+dotnet restore TestWebApi/TestApi/TestApi.csproj
+dotnet restore TestWebApi/TestApi.Tests/TestApi.Tests.csproj
