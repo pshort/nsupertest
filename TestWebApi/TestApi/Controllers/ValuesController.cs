9926c476ba3962ac0d1fabc1b525d1a9f80db87f
Peter Short <peter.short@gmail.com>
2020-03-11 08:41:05 +0000

    Updated to 3.1 + fixed netcore config
    Netcore config loader wasnt actually working because the base class
    constructor called start server before the assignment. Small refactor
    and upgrade to 3.1 for some projects.

--- TestWebApi/TestApi/Controllers/ValuesController.cs
+++ TestWebApi/TestApi/Controllers/ValuesController.cs
@@ -3,42 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace TestApi.Controllers
 {
-    [Route("api/[controller]")]
-    public class ValuesController : Controller
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
     {
-        // GET api/values
+        private readonly IEnumerable<string> values;
+        public ValuesController(IConfiguration config)
+        {
+             values = config.GetSection("Values").Get<IEnumerable<string>>();
+        }
+
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            return values;
         }
     }
 }
--- TestWebApi/TestApi/Program.cs
+++ TestWebApi/TestApi/Program.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace TestApi
 {
@@ -12,14 +8,14 @@ namespace TestApi
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
-
-            host.Run();
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
